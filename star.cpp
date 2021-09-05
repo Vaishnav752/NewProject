40a849a5c52bfccba53314b8e608cc98fbf048c7
Vaishnav752 <vrjade07052002@gmail.com>
2021-09-05 11:27:04 +0530

    first comment

---
+++ star.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i, j, k, n;
+	
+	cout<<"Enter the number of rows: ";
+	cin>>n;
+	
+	for(i=1;i<=n;i++)
+	{
+		for(k=n-i;k>0;k--)
+		cout<<" ";
+		for(j=1;j<=i;j++)
+		cout<<"* ";
+		cout<<endl;
+	}
+	return 0;
+	
+	
+}
